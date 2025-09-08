ba5b0d6243adb3b21e6bbe1149b0b5608ef3e3db
Jairo Miranda <jairo@SSAP-N30063131.fieg.com.br>
2025-09-08 00:51:08 -0300

    tentativa de ssl

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Carrega os certificados Let's Encrypt
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/tds.sisnet.app/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/tds.sisnet.app/fullchain.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   // registra adapter ws
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000); // servidor HTTP + WS no mesmo processo
-  console.log('App rodando na porta 3000 (ws://localhost:3000)');
+  await app.listen(7070); // servidor HTTP + WS no mesmo processo
+  console.log('App rodando na porta 7070 (ws://localhost:7070)');
 }
 bootstrap().catch((err) => {
   console.error('Erro ao iniciar a aplicação:', err);
